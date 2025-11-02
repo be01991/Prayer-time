97d1b6927f809b0bda46616377de6f41dc250f86
be01991 <fooadbe@gmail.com>
2025-11-02 21:49:25 +0000

    second
    second commit

--- script.js
+++ script.js
@@ -2,22 +2,28 @@ let hrs = document.getElementById('hrs');
 let min = document.getElementById('min');
 let sco = document.getElementById('sco');
 let day = document.getElementById('day');
+// let ampm = document.getElementById('mapm');
 let month = document.getElementById('month');
 let Year = document.getElementById('year');
 let hDay = document.getElementById('hDay');
 let hMonth = document.getElementById('hMonth');
 let hYear = document.getElementById('hYear');
 
-let months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
-let days = ['SUN','MON', 'TUS', 'WED', 'THR' ,'FRY', 'SAT'];
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+let days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday' ,'Friday', 'Saturday'];
 
 function update(){
     let currentTime = new Date();
 
-    hrs.innerHTML= currentTime.getHours();
+    
+    hrs.innerHTML= (currentTime.getHours()<10?'0':'') + currentTime.getHours();
+    // ampm.innerHTML = hrs >= 12 ? 'PM' : 'AM';
+    // hrs.innerHTML = hrs % 12;
+    // hrs.innerHTML = hrs ? hrs : 12;
     min.innerHTML = (currentTime.getMinutes()<10?'0':'') + currentTime.getMinutes();
     sco.innerHTML = (currentTime.getSeconds()<10?'0':'') + currentTime.getSeconds();
     day.innerHTML = days[currentTime.getDay()];
+    output.innerHTML = currentTime.getDate();
     month.innerHTML = months[currentTime.getMonth()];
     Year.innerHTML = currentTime.getFullYear(); 
     getIslamicDate();
@@ -47,7 +53,7 @@ window.addEventListener("load", function(){
     setInterval( update, 1000);
 });
 
-fetch("https://api.aladhan.com/v1/timingsByCity?city=Cork&country=Ireland&method=2")
+fetch("https://api.aladhan.com/v1/timingsByCity?city=Tralee&country=Ireland&method=2")
   .then(response => response.json())
   .then(data => {
     document.getElementById("fajr").innerHTML = 'Fajr'+' '+ data.data.timings.Fajr;
